6bc0c620b729ebe20ba4ad6d80a32b6febdeb855
Mayasa79 <mayasa.sabie@gmail.com>
2020-11-10 11:26:30 -0600

    commit

---
+++ src/hELLO.java
@@ -0,0 +1,8 @@
+import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
+
+public class hELLO {
+    public static void main(String[] args) {
+        System.out.println("HIJJJ");
+        // mac user and windows are happy
+    }
+}
--- src/main/main.java
+++ src/main/main.java
@@ -5,7 +5,8 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        //B21 is a good at Intellige
     }
-    
+
 
 }
